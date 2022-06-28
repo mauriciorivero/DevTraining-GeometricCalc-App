2f6e48777272154cafc1978e6c08989c9a4891d3
Mauricio Rivero <rivero.mauricio@gmail.com>
2022-06-28 07:46:00 -0500

    Added the square calculations in js file

---
+++ js/GeometricApp.js
@@ -0,0 +1,10 @@
+//square geometric calculations
+const squareSide = 5;
+console.log("The sides of the square masure: "+squareSide+"cm");
+
+const squarePerimeter = squareSide * 4;
+console.log("The perimeter of the square measure: "+squarePerimeter+"cm");
+
+const squareArea = squareSide * squareSide;
+console.log("The square area measure: "+squareArea+"cm^2");
+
